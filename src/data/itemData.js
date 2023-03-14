36bc8be8737e57b8650ae67cd2a69132fbc7d60b
Falak0891 <70525535+Falak0891@users.noreply.github.com>
2023-03-14 16:45:42 +0530

    Update itemData.js

--- src/data/itemData.js
+++ src/data/itemData.js
@@ -1,4 +1,4 @@
-export const items = [
+export const ITEMS = [
   { id: "i-1", title: "Item1", detail: "Item-1-Details" },
   { id: "i-2", title: "Item2", detail: "Item-2-Details" },
   { id: "i-3", title: "Item3", detail: "Item-2-Details" },
